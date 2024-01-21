b0d5e89900353995973b0dc501150b2bd74c3255
Bateyjosue <josuebatey19@gmail.com>
2024-01-21 22:32:31 +0200

    feat: add hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 
 function App() {
 
   return (
-    <div className="max-h-full h-dvh min-h-full bg-[#F8F8F8]">
-      <Header/>
+    <div className="max-h-full h-dvh min-h-full bg-[#F8F8F8] poppins-regular">
+      <Header />
+      <main className="wrapper-md lg:wrapper-lg">
+        <Hero />
+      </main>
     </div>
   )
 }
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,2 @@
 export * as logoColor from './img/logo-color.png';
+export * as heroImage from './img/hero.png';

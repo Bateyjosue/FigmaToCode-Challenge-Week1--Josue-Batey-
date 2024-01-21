27cc6e0480b14a85031debc8159c4a40a38af53c
Bateyjosue <josuebatey19@gmail.com>
2024-01-21 23:53:54 +0200

    feat: add mobile breakpoint to here section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,18 +4,18 @@ import { heroImage } from "../assets"
 
 function Hero() {
   return (
-    <section className="poppins-regular grid lg:grid-cols-2 mt-8 place-items-center">
-      <div className="flex flex-col">
-        <h4 className="text-secondary font-semibold text-xl">Dr. Matthew anderson</h4>
-        <h2 className="poppins-bold text-5xl font-bold">A dedicated doctor you can trust</h2>
+    <section className="poppins-regular grid lg:grid-cols-2 lg:mt-8 lg:place-items-center">
+      <div className="flex flex-col text-center lg:text-left">
+        <h4 className="text-secondary font-semibold lg:text-xl">Dr. Matthew anderson</h4>
+        <h2 className="poppins-bold text-3xl lg:text-5xl font-bold">A dedicated doctor you can trust</h2>
         <p className="text-iris my-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ut nemo error placeat accusantium provident velit</p>
         <Button
           text="Book appointment"
-          className="bg-primary flex gap-x-4 px-4 py-2 rounded-full font-semibold text-white w-1/2">
-          <MessageIcon className="h-6 w-6"/>
+          className="bg-primary flex gap-x-4 py-2 rounded-full font-semibold text-white w-2/3 mx-auto lg:w-1/2 items-center justify-center">
+          <MessageIcon className="h-5 w-5"/>
         </Button>
       </div>
-      <div className="flex justify-end">
+      <div className="flex mt-4 lg:mt-0 lg:justify-end">
         <img src={heroImage.default} alt="" className="-mr-16" />
       </div>
     </section>

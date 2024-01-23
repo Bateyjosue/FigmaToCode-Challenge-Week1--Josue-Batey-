7c4521be5ebf718ba3e75d5c505047613c85a7ea
Bateyjosue <josuebatey19@gmail.com>
2024-01-23 22:55:03 +0200

    fix: paragrphe text size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ function Hero() {
       <div className="order-10 lg:order-0 flex flex-col text-center lg:text-left">
         <h4 className="text-secondary text-2xl mb-2 lg:font-semibold lg:text-xl">Dr. Matthew anderson</h4>
         <h2 className="poppins-bold text-3xl lg:text-5xl font-bold">A dedicated doctor you can trust</h2>
-        <p className="text-iris my-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ut nemo error placeat accusantium provident velit</p>
+        <p className="text-iris text-lg my-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum eget vel, nunc nulla feugiat. Metus ut.</p>
         <Button
           text="Book appointment"
           className="bg-primary mb-8 lg:mb-0 flex gap-x-4 py-2 rounded-full font-semibold text-white w-2/3 mx-auto lg:mx-0 lg:w-1/2 items-center justify-center">
--- src/components/TextComponent.tsx
+++ src/components/TextComponent.tsx
@@ -15,7 +15,7 @@ function TextComponent({title, headline, paragraphe,className,classNamePara}: Pr
     <div className="flex flex-col">
         <h4 className="text-secondary text-2xl lg:font-semibold lg:text-xl">{title}</h4>
         <h2 className={`poppins-bold font-semibold lg:font-bold ${className}`}>{headline}</h2>
-        <p className={`text-iris my-4 ${classNamePara}`}>{paragraphe}</p>
+        <p className={`text-iris text-lg my-4 ${classNamePara}`}>{paragraphe}</p>
         <Button
           text="Book an appointment"
           className="bg-primary flex gap-x-4 py-3 px-4 lg:px-0 rounded-full font-semibold text-white w-2.5/3 mx-auto lg:mx-0 lg:w-1/2 items-center justify-center">

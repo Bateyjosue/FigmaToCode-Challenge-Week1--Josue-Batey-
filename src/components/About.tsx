46cfe34a0a1d188c89c877c727d274ab9656e008
Bateyjosue <josuebatey19@gmail.com>
2024-01-23 22:20:04 +0200

    feat: update about section mobile

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,11 @@
 import WhatAppIcon from "../assets/icons/WhatAppIcon"
 import DocImage from '../assets/img/about-doc.png'
-import DocBg from '../assets/img/doc2/plusicons.png'
 import TextComponent from "./TextComponent"
 
 function About() {
   return (
-    <section className="wrapper-md lg:wrapper-lg grid grid-cols-1 lg:grid-cols-2 lg:place-content-center gap-x-16 lg:py-32 border bg-white">
-      <div className={`bg-[url(${DocBg})]`}>
+    <section className="wrapper-md lg:wrapper-lg grid grid-cols-1 lg:grid-cols-2 lg:place-content-center gap-x-16 pb-16 lg:pb-0 lg:py-32 bg-white">
+      <div className="">
         <img src={DocImage} alt="" />
       </div>
       <div className="flex items-center">
@@ -14,7 +13,7 @@ function About() {
           title="About me"
           headline="A dedicated doctor with the core mission to help"
           paragraphe="orem ipsum dolor sit amet, consectetur adipiscing elit. Quam proin nibh cursus at sed sagittis amet, sed. Tristique id nibh lobortis nunc elementum. Tellus quam mauris aenean turpis vulputate sodales nullam lobortis. Vulputate tortor tincidun."
-          className="text-3xl lg:text2xl py-2"
+          className="text-2xl lg:text-3xl lg:text2xl py-2"
           classNamePara="text-sm"
         >
             <WhatAppIcon/>
--- src/components/TextComponent.tsx
+++ src/components/TextComponent.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from 'react'
 import Button from './Button'
-import MessageIcon from '../assets/icons/MessageIcon'
+import WhatAppIcon from '../assets/icons/WhatAppIcon'
 
 interface TextComponentProps {
   title: string
@@ -12,14 +12,14 @@ interface TextComponentProps {
 
 function TextComponent({title, headline, paragraphe,className,classNamePara}: PropsWithChildren<TextComponentProps>) {
   return (
-    <div className="flex flex-col text-center lg:text-left">
-        <h4 className="text-secondary font-semibold lg:text-xl">{title}</h4>
-        <h2 className={`poppins-bold font-bold ${className}`}>{headline}</h2>
+    <div className="flex flex-col">
+        <h4 className="text-secondary text-2xl lg:font-semibold lg:text-xl">{title}</h4>
+        <h2 className={`poppins-bold font-semibold lg:font-bold ${className}`}>{headline}</h2>
         <p className={`text-iris my-4 ${classNamePara}`}>{paragraphe}</p>
         <Button
-          text="Book appointment"
-          className="bg-primary flex gap-x-4 py-2 rounded-full font-semibold text-white w-2/3 mx-auto lg:mx-0 lg:w-1/2 items-center justify-center">
-          <MessageIcon className="h-5 w-5"/>
+          text="Book an appointment"
+          className="bg-primary flex gap-x-4 py-3 px-4 lg:px-0 rounded-full font-semibold text-white w-2.5/3 mx-auto lg:mx-0 lg:w-1/2 items-center justify-center">
+          <WhatAppIcon/>
         </Button>
       </div>
   )

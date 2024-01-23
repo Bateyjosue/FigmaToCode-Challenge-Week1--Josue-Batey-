0829d2a86761281e41a2db60102d7af4f5f22eb1
Bateyjosue <josuebatey19@gmail.com>
2024-01-23 22:04:49 +0200

    feat: update design hero section mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,21 +8,21 @@ import HealthIcon from "../assets/icons/HealthIcon"
 
 function Hero() {
   return (
-    <section className="wrapper-md lg:wrapper-lg grid lg:grid-cols-2 lg:place-items-center relative rounded-b-2xl pb-32 lg:pb-24">
-      <div className="flex flex-col text-center lg:text-left">
-        <h4 className="text-secondary font-semibold lg:text-xl">Dr. Matthew anderson</h4>
+    <section className="wrapper-md lg:wrapper-lg grid lg:grid-cols-2 lg:place-items-center relative rounded-b-2xl pb-0 lg:pb-24">
+      <div className="order-10 lg:order-0 flex flex-col text-center lg:text-left">
+        <h4 className="text-secondary text-2xl mb-2 lg:font-semibold lg:text-xl">Dr. Matthew anderson</h4>
         <h2 className="poppins-bold text-3xl lg:text-5xl font-bold">A dedicated doctor you can trust</h2>
         <p className="text-iris my-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ut nemo error placeat accusantium provident velit</p>
         <Button
           text="Book appointment"
-          className="bg-primary flex gap-x-4 py-2 rounded-full font-semibold text-white w-2/3 mx-auto lg:mx-0 lg:w-1/2 items-center justify-center">
+          className="bg-primary mb-8 lg:mb-0 flex gap-x-4 py-2 rounded-full font-semibold text-white w-2/3 mx-auto lg:mx-0 lg:w-1/2 items-center justify-center">
           <MessageIcon className="h-5 w-5"/>
         </Button>
       </div>
-      <div className="flex mt-4 lg:mt-0 lg:justify-end">
+      <div className="order-5 lg:order-10 flex mt-4 mb-10 lg:mb-0 lg:mt-0 lg:justify-end">
         <img src={heroImage.default} alt="" className="-mr-16" />
       </div>
-      <div className="wrapper-md lg:wrapper-lg lg:absolute flex flex-col lg:flex-row gap-6 -bottom-48 lg:-bottom-12 lg:left-20 lg:right-20">
+      <div className=" order-10 wrapper-md lg:wrapper-lg lg:absolute flex flex-col lg:flex-row gap-6 -bottom-20 lg:-bottom-12 lg:left-20 lg:right-20">
         <Advert
           title="24 hour service"
           paragraphe="Lorem ipsum dolor sit amet, consectetur adipscing elit."
